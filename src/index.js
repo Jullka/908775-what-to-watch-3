2d20c90259c596f6395d00e89edf724d04931dbe
Jullka <dj-1@ukr.net>
2020-02-06 18:32:18 +0200

    Добавляет задание 1-3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const MovieCardDesk = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  releaseData: 2014
+};
+
+ReactDOM.render(
+    <App
+      title={MovieCardDesk.title}
+      genre={MovieCardDesk.genre}
+      releaseData={MovieCardDesk.releaseData}
+    />,
+    document.querySelector(`#root`)
+);
 
