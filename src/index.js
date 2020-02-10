ef0285ffd7287cd3e73e59c4d2ca4f7d46e973f1
Jullka <dj-1@ukr.net>
2020-02-11 00:39:03 +0200

    Добавляет задание 2-1

--- src/index.js
+++ src/index.js
@@ -2,17 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-const movieCardDesk = {
+const movieCard = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
-  releaseData: 2014
+  releaseData: 2014,
 };
+
+const moviesList = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
 
 ReactDOM.render(
     <App
-      title={movieCardDesk.title}
-      genre={movieCardDesk.genre}
-      releaseData={movieCardDesk.releaseDate}
+      title={movieCard.title}
+      genre={movieCard.genre}
+      releaseData={movieCard.releaseDate}
+      moviesList={moviesList}
     />,
     document.querySelector(`#root`)
 );
